e31702e3c8e5a9d79ea68522a53dacf82bd27deb
jjkip <jjlangatjose@gmail.com>
2023-05-30 23:18:19 +0300

    Looping though an array in a list in Swift UI

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pepople = ["John", "Kim", "Mary", "Chela", "Kiprop", "Achieng",]
+    let people = ["John", "Kim", "Mary", "Chela", "Kiprop", "Achieng",]
     var body: some View {
-        List(0..<5){
-            Text("Dynamic row \($0)")
+        List {
+            Text("Static row")
+            
+            ForEach(people, id: \.self){
+                Text($0)
+            }
+            Text("Static row")
+//        List(0..<5){
+//            Text("Dynamic row \($0)")
 //            Section("Section 1") {
 //                Text("Static row 1")
 //                Text("Static row 2")
