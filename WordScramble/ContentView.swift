89bdedb5bc89c4459f1c93f425d38ad648c6c1dd
jjkip <jjlangatjose@gmail.com>
2023-05-30 23:15:53 +0300

    looping thought a list range

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pepople = ["John", "Kim", "Mary", "Chela", "Kiprop", "Achieng",]
     var body: some View {
-        List {
-            Section("Section 1") {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-            Section("Section 2") {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
-                }
-            }
-            Section("Section 3"){
-                Text("Static row 3")
-                Text("Static row 4")
-            }
+        List(0..<5){
+            Text("Dynamic row \($0)")
+//            Section("Section 1") {
+//                Text("Static row 1")
+//                Text("Static row 2")
+//            }
+//            Section("Section 2") {
+//                ForEach(0..<5) {
+//                    Text("Dynamic row \($0)")
+//                }
+//            }
+//            Section("Section 3"){
+//                Text("Static row 3")
+//                Text("Static row 4")
+//            }
         }
         .listStyle(.grouped)
     }
