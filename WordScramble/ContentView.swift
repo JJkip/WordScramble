de93c60044d5aaa61069661f7090d3bc408cdf20
jjkip <jjlangatjose@gmail.com>
2023-05-31 23:02:16 +0300

    Loading resources from your app bundle in Swift UI

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -35,6 +35,13 @@ struct ContentView: View {
         }
         .listStyle(.grouped)
     }
+    func loadFile(){
+        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+            if let fileContents = try? String(contentsOf: fileURL) {
+                fileContents
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
